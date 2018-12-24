e0bc5257751ac23af23894423f4a31e89acf437a
afleischhauer <alexander.fleischhauer@gmail.com>
2018-12-25 00:29:22 +0100

    4.2 added price & color field to products

---
+++ db/migrate/20181224225921_add_price_tag.rb
@@ -0,0 +1,5 @@
+class AddPriceTag < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :price, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_090411) do
+ActiveRecord::Schema.define(version: 2018_12_24_225921) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_12_24_090411) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "colour"
+    t.integer "price"
   end
 
 end
