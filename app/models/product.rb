516e4f8fd16a2e243e141b19e7c4a70e6c01a851
afleischhauer <alexander.fleischhauer@gmail.com>
2019-01-10 14:06:37 +0100

    reviews 5.2/3/4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ApplicationRecord
-end
 
-def self.search(search_term)
-  if Rails.env.production?
-     Product.where("name ILIKE ?", "%#{search_term}%")
-  else
-    Product.where("name LIKE ?", "%#{search_term}%")
+
+  def self.search(search_term)
+    if Rails.env.production?
+      Product.where("name ILIKE ?", "%#{search_term}%")
+    else
+      Product.where("name LIKE ?", "%#{search_term}%")
+    end
   end
+
+  
 end
