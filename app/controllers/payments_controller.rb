b0e33dd37cfc3ce3412c0009c004fcb598e295ad
afleischhauer <alexander.fleischhauer@gmail.com>
2019-01-21 01:47:50 +0100

    6.7. debugging

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,7 @@ class PaymentsController < ApplicationController
 
         token = params[:stripeToken]
 
-        # Create the charge on Stripe's servers - this will charge the user's card
+        # this will charge the user's card
         begin
           charge = Stripe::Charge.create(
             amount: (@product.price * 100).to_i,
@@ -27,7 +27,7 @@ class PaymentsController < ApplicationController
           end
 
         rescue Stripe::CardError => e
-          # The card has been declined
+          # card declined
           body = e.json_body
           err = body[:error]
           flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
