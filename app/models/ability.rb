e261489a679227685b2b603c1ce1c437ce83b9e9
afleischhauer <alexander.fleischhauer@gmail.com>
2019-01-26 21:34:05 +0100

    ability fix2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user
+      can :manage, User, id: user.id
+      if user.admin?
         can :manage, :all
-      else
-        can :manage, User, id: user.id
+      end
     end
   end
-
 end
